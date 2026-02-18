f93d1ff942cb09871caecd89d1188bc8e9f2a157
Tausif Khan <tautumhare@gmail.com>
2026-02-18 15:00:36 +0530

    yeee

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,18 +3,10 @@ import SandwichScroll from "../components/home/SandwichScroll";
 import MenuSection from "../components/home/MenuSection";
 import TestimonialSection from "../components/home/Testimonial";
 import Footer from "../components/home/Footer";
-import { Playfair_Display, Poppins } from "next/font/google";
-
-const serif = Playfair_Display({
-  subsets: ["latin"],
-  weight: ["700"],
-  style: ["italic", "normal"],
-});
-const sans = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export default function Home() {
   return (
-    <main className={`${sans.className} bg-[#E8E1CF] antialiased`}>
+    <main className="bg-[#E8E1CF] antialiased font-sans">
       {/* GLOBAL NAVIGATION */}
       <Navbar />
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./components/usermenu/Providers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased font-sans">
         {/* make changes on this but do not know why and i am also not intersed to know*/}
         <Providers>
           {children}
