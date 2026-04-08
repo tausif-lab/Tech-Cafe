c70ed7c237da5661e3836a584b542a9d26dbe84d
Tausif Khan <tautumhare@gmail.com>
2026-04-08 13:47:12 +0530

    localhost aboart

--- lib/firebase/client.ts
+++ lib/firebase/client.ts
@@ -1,15 +1,22 @@
 import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getMessaging, getToken, onMessage, Messaging } from 'firebase/messaging'
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
   appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+}*/
+const firebaseConfig = {
+  apiKey:            'AIzaSyDSFgoP3WepTqGImh5d4tpUhyl29UNJQXI',
+  authDomain:        'tech-cafe-40e07.firebaseapp.com',
+  projectId:         'tech-cafe-40e07',
+  storageBucket:     'tech-cafe-40e07.firebasestorage.app',
+  messagingSenderId: '819385560560',
+  appId:             '1:819385560560:web:f9b89e12e370fc189b8135',
 }
-
 // Singleton Firebase app — safe to call multiple times
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
 
